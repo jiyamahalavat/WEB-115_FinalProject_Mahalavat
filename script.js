55b677ffb25009ad02165a6eeaeaad90a4ad7dc7
jiyamahalavat <jiyamahalavat@gmail.com>
2026-04-28 14:07:36 -0400

    update script.js
    Introduced a Task class to encapsulate task properties and added a function to retrieve task input values.

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const taskList = document.getElementById("taskList");
-const progressText = document.getElementById("progress");
-const name = document.getElementById("taskName").value;
-const dueDate = document.getElementById("dueDate").value;
-const priority = document.getElementById("priority").value;
+class Task {
+constructor(name, dueDate, priority){
+    this.name = name;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.completed = false; //will change to true if the task is completed
+}
+};
+
+const taskList = document.getElementById("taskList"); //gets taskList 
+const progressText = document.getElementById("progress"); // shows progress in tasks over time
+
+function addTasks(){ // make function that will add all tasks to do to dashboard
+    const name = document.getElementById("taskName").value;
+    const priority = document.getElementById("priority").value;
+    const dueDate = document.getElementById("dueDate").value;
+}
+
+
+//can use JSON.parse 
+//Storage.setItem() -> setItem(keyName, keyValue)
+// used like this -> localStorage.setItem("image", "myCat.png")
+//there is a fetch API which uses the fetch() method 
