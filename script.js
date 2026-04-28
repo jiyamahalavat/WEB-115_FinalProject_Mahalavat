f90ec8159435b67f568ea7294dded49897b9c80f
jiyamahalavat <jiyamahalavat@gmail.com>
2026-04-28 13:58:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const taskList = document.getElementById("taskList");
 const progressText = document.getElementById("progress");
+const name = document.getElementById("taskName").value;
+const dueDate = document.getElementById("dueDate").value;
+const priority = document.getElementById("priority").value;
