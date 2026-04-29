3643badb2aa79f0a2d4ed6d740050d922b22745b
jiyamahalavat <jiyamahalavat@gmail.com>
2026-04-29 13:07:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,19 @@ constructor(name, dueDate, priority){
 const taskList = document.getElementById("taskList"); //gets taskList 
 const progressText = document.getElementById("progress"); // shows progress in tasks over time
 
+document.getElementById("addTaskBtn").addEventListener("click", addTask);
+
 function addTasks(){ // make function that will add all tasks to do to dashboard
     const name = document.getElementById("taskName").value;
     const priority = document.getElementById("priority").value;
     const dueDate = document.getElementById("dueDate").value;
+    
+    if (name === "") return;
+
+  const newTask = new Task(name, dueDate, priority);
+  tasks.push(newTask);
+
+ 
 }
 
 
