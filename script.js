2baeec69b1f1bd9ff0a142a22ca9a8108dbd583d
jiyamahalavat <jiyamahalavat@gmail.com>
2026-04-29 14:07:27 -0400

    update script.js
    Updated task addition logic to check for empty task name and corrected variable usage.

--- script.js
+++ script.js
@@ -10,19 +10,17 @@ constructor(name, dueDate, priority){
 const taskList = document.getElementById("taskList"); //gets taskList 
 const progressText = document.getElementById("progress"); // shows progress in tasks over time
 
-document.getElementById("addTaskBtn").addEventListener("click", addTask);
+document.getElementById("addTaskBtn").addEventListener("click", addTasks ); //event listener for button that adds tasks
 
 function addTasks(){ // make function that will add all tasks to do to dashboard
     const name = document.getElementById("taskName").value;
     const priority = document.getElementById("priority").value;
     const dueDate = document.getElementById("dueDate").value;
-    
-    if (name === "") return;
 
-  const newTask = new Task(name, dueDate, priority);
-  tasks.push(newTask);
+    if (name == " "){
+        const newTask = new Task(name, dueFDpriority); //object that creates new task based on what the user enters 
+    }
 
- 
 }
 
 
